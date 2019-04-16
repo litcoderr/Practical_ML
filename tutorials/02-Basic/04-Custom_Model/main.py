6c957300f418898cb65b20ca539288c0f074d84e
James Youngchae Chee <litcoderr@gmail.com>
2019-04-16 10:29:36 +0900

    Made custom layer tutorial so far --> Need to add custom model tutorial

--- tutorials/02-Basic/04-Custom_Model/main.py
+++ tutorials/02-Basic/04-Custom_Model/main.py
@@ -1,4 +1,52 @@
 import tensorflow as tf
 
+# ================================================================== #
+#                         Table of Contents                          #
+# ================================================================== #
+
+# 1. Non-Custom Layers
+# 2. Custom Layers
+
+# =================================================================== #
+
+# 1. Non-Custom Layers
+non_custom_model = tf.keras.Sequential([
+    tf.keras.layers.Flatten(input_shape=(28, 28)),
+    tf.keras.layers.Dense(128, activation='relu'),
+    tf.keras.layers.Dense(10, activation='softmax')
+])
+
+
+# 2. Custom Layers
+class CustomLayer(tf.keras.layers.Layer):
+    def __init__(self, num_output):
+        super(CustomLayer, self).__init__()
+        self.n_output = num_output
+
+    def build(self, input_shape):
+        self.kernel = self.add_variable("kernel",
+                                        shape=[int(input_shape[-1]), self.n_output])
+
+    def call(self, input):
+        return tf.matmul(input, self.kernel)
+
+
+custom_layer_model = tf.keras.Sequential([
+    tf.keras.layers.Flatten(input_shape=(28,28)),
+    CustomLayer(10),
+    tf.keras.layers.ReLU()
+])
+
+# 3. Custom Model
+
+
 if __name__ == '__main__':
-    pass
+    input = tf.Variable(tf.ones((1, 28, 28)))
+
+    # 1. Running Non-Custom Layers
+    output = non_custom_model(input)
+    print("non_custom_model_result: {}".format(output.shape))
+
+    # 2. Running Custom Layers
+    output = custom_layer_model(input)
+    print("output of custom layer model : {}".format(output.shape))
